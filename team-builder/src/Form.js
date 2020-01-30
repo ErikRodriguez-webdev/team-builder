eeb7c217fae4a6aec2d1edf252871aed8006b4d0
Erik Rodriguez <eric19.21.03@gmail.com>
2020-01-29 16:06:34 -0800

    Add submit and change handlers to form
    This was to save inputs from user and prevent default refresh

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,4 +1,17 @@
 import React, { useState } from "react";
+import App from "./App";
+
+function CardMaker(obj) {
+  return (
+    <div>
+      <div>
+        <h2>Name: {obj.name}</h2>
+        <h2>Email: {obj.email}</h2>
+        <h2>Role: {obj.role}</h2>
+      </div>
+    </div>
+  );
+}
 
 function Form() {
   //Members will populate here
@@ -12,27 +25,52 @@ function Form() {
   ]);
   console.log(memberList);
 
+  //User info handler here
+  const changeHandler = (event) => {
+    setMemberList({ ...memberList, [event.target.name]: event.target.value });
+  };
+
+  //Submit handler here
+  const submitHandler = (event) => {
+    event.preventdefault();
+  };
+
   //Form layout is here
   return (
-    <form>
-      <label htmlFor="name">Full Name: </label>
-      <input name="name" type="text" placeholder="First Name" />
-      <br />
+    <>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">Full Name: </label>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={changeHandler}
+        />
+        <br />
 
-      <label htmlFor="email">Email: </label>
-      <input name="email" type="email" placeholder="Email" />
-      <br />
+        <label htmlFor="email">Email: </label>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={changeHandler}
+        />
+        <br />
 
-      <label htmlFor="role">Select Role: </label>
-      <select>
-        <option value="front end">Front End</option>
-        <option value="back end">Back End</option>
-        <option value="full stack">Full Stack</option>
-      </select>
-      <br />
+        <label htmlFor="role">Select Role: </label>
+        <select name="role" onChange={changeHandler}>
+          <option value="front end">Front End</option>
+          <option value="back end">Back End</option>
+          <option value="full stack">Full Stack</option>
+          <option value="full stack">Full Stack</option>
+          <option value="ui designer">UI Designer</option>
+        </select>
+        <br />
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+      <App CardMaker={CardMaker} />
+    </>
   );
 }
 
